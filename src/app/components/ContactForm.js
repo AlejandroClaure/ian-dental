fcbbb02030c1bf10da14d9eeaa2df8365448087b
AlejandroClaure <alejandroclaure01@gmail.com>
2025-04-01 15:44:15 -0300

    modified message forms from contact

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -26,7 +26,7 @@ export default function ContactForm() {
     }));
   };
   //funcion para agregar el mail al final del mensaje
-  const fullMessage = `${formData.message}\n${formData.email}`;
+  const fullMessage = `${formData.email}\n${formData.inquiry}\n${cartItems}\n\nAgradezco de antemano su respuesta.\nSaludos cordiales,\n${formData.name}`;
 
   
   const handleSubmit = async (e) => {
@@ -48,8 +48,6 @@ export default function ContactForm() {
         'service_iandental', // Service_ID proporcionado
         'template_11q7y4n', // Template_ID proporcionado
         {
-          name: formData.name,
-          email: formData.email,
           message: fullMessage,
         }
       );
